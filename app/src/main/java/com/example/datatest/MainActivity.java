5337094787a2a2dc0f4527e59a9328523315958f
DevAZI <80841284+DevAZI@users.noreply.github.com>
2021-11-09 12:13:19 +0900

    파일 생성 1

--- app/src/main/java/com/example/datatest/MainActivity.java
+++ app/src/main/java/com/example/datatest/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.datatest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnWrite = findViewById(R.id.button);
+        Button btnRead = findViewById(R.id.button2);
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileOutputStream outputStream = openFileOutput("file.txt", Context.MODE_PRIVATE);//file.txt를 만듬 있으면 그거 사용  /  Context.MODE_PRIVATE= 나만 접근 가능한 것
+                    String str = "파일 테스트";
+                    outputStream.write(str.getBytes());
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
